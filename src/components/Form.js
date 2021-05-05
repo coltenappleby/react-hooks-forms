15f25b383cbc3b8446b1864837237cb8068c2b40
coltenappleby <colten.appleby@gmail.com>
2021-05-04 20:34:38 -0400

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,22 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  function handleFirstNameChange(event) {
+    event.preventDefault()
+    
+    setFirstName(event.target.value)
+    console.log(firstName)
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value)
+  }
+
+ 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input onChange = {handleFirstNameChange}  type="text" value={firstName} />
+      <input onChange = {handleLastNameChange} type="text" value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
